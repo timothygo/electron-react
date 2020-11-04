d1c9bc8408f7eaaf001ae18ec36c43bc033da0f5
Timothy Go <46599593+timothygo@users.noreply.github.com>
2020-11-04 15:00:01 +0800

    Update electron.js

--- public/electron.js
+++ public/electron.js
@@ -14,6 +14,7 @@ app.on("ready", () => {
     resizable: config.resizable,
     show: config.show,
     frame: config.frame,
+    transparent: config.transparent
     webPreferences: {
       nodeIntegration: false,
       devTools: isDev,
